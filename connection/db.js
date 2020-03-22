9df323cf60ff1c1ab771be22a98c3f8b2bf2b9f0
Sam1996 <samnishanth01@gmail.com>
2020-03-22 10:13:21 +0530

    fixed S in sequelize

--- connection/db.js
+++ connection/db.js
@@ -1,10 +1,5 @@
 const { Sequelize, Op } = require("sequelize");
 
-// const sequelize = new Sequelize("coronaupdateindia", "root", "", {
-// 	host: "localhost",
-// 	dialect: "mysql"
-// });
-
 const sequelize = new Sequelize("harvea94_corona", "harvea94_sam", "W3lcome!", {
 	host: "harvestsavepeople.in",
 	dialect: "mysql"
--- models/aboutcorona.js
+++ models/aboutcorona.js
@@ -1,4 +1,4 @@
-const Sequelize = require("Sequelize");
+const Sequelize = require("sequelize");
 
 module.exports = sequelize.define(
 	"aboutcorona",
